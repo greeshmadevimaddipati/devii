bc846c70c3bb94557ce5be6fcfd1e0d4be77bad0
greeshmadevimaddipati <46023172+greeshmadevimaddipati@users.noreply.github.com>
2018-12-21 17:42:35 +0530

    Add files via upload

---
+++ BIGGER.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1,num2;
+clrscr();
+printf("enter number1");
+scanf("%d",&num1);
+printf("enter number2");
+scanf("%d",&num2);
+// printf("%d",num1>num2);
+if(num1>num2)
+{
+ printf(" num1 is bigger");
+}
+else
+{
+printf("num2 is bigger");
+}
+getch();
+ }
+
+
+
